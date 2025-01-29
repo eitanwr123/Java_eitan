748744236964b49873876f2f9afbee22797331fc
eitanwr123 <eitanwr@gmail.com>
2025-01-29 19:07:45 +0200

    tomer

--- class_3/index.js
+++ class_3/index.js
@@ -1,1 +1,8 @@
-console.log("Eitan Werhimer Shoham")
+console.log("Eitan Werthimer Shoham")
+
+let firstname = "Eitan"
+let lastName = "Werthimer"
+let age = 23
+
+fullName = `${firstname} ${lastName}...`
+console.log(fullName)
