97be8034ee021b1cd799b87959ce318df5af990a
eitanwr123 <eitanwr@gmail.com>
2025-01-29 19:22:32 +0200

    a

--- class_3/index.js
+++ class_3/index.js
@@ -6,3 +6,13 @@ let age = 23
 
 fullName = `${firstname} ${lastName}...`
 console.log(fullName)
+
+let email = "eitanwr@gmail.com"
+let job = "analyst"
+let isWorking = true
+let hasACar = true
+let eitanData = `${firstname} ${lastName} ${email} ${job} ${isWorking} ${hasACar}`
+console.log(eitanData)
+
+
+
