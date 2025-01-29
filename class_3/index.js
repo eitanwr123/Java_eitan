b5dc61986c8222cdec6be26f714ae7b4e62312a7
eitanwr123 <eitanwr@gmail.com>
2025-01-29 20:45:01 +0200

    b

--- class_3/index.js
+++ class_3/index.js
@@ -13,6 +13,30 @@ let isWorking = true
 let hasACar = true
 let eitanData = `${firstname} ${lastName} ${email} ${job} ${isWorking} ${hasACar}`
 console.log(eitanData)
+console.log();
+
+if (isWorking === true) {
+    console.log("working...");
 
 
+} else {
+    console.log("not working...")
+}
+
+//if the car is type 'mazda' and color 'red' and model 'cx5' - print "Yes i found my Car!"
+//otherwise print "I dont need this Car!"
+
+console.log("Ex 3")
+
+const carType = prompt("car type")
+const carColor = prompt("color")
+const carModel = prompt("model")
+
+if (carType === "mazda" && carColor === "red" && carModel === "cx5") {
+    console.log("Yes i found my Car!");
+
+
+} else {
+    console.log("I dont need this Car!");
+}
 
